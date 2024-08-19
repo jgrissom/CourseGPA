b245030040892742a0a2b78738b8df7ae29269c5
jgrissom <jgrissom91@gmail.com>
2024-08-19 15:54:57 -0500

    [1.02] Input course names / grades - for loop

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int i;
+for (i = 0; i < 7; i++)
+{
+    // ask a question
+    Console.WriteLine("Enter a course (Y/N)?");
+    // input the response
+    string? resp = Console.ReadLine()?.ToUpper();
+    // if the response is anything other than "Y", stop asking
+    if (resp != "Y")
+    {
+        break;
+    }
+    // prompt for course name
+    Console.WriteLine("Enter the course name.");
+    Console.ReadLine();
+    // prompt for course grade
+    Console.WriteLine("Enter the course grade.");
+    Console.ReadLine();
+}
